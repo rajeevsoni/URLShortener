ee4103b687cfa2040d7afe439856d0b46e9a9739
Rajeev Soni <rajeev.soni@onsolve.com>
2019-12-10 23:59:09 +0530

    initial commit

---
+++ URLShortener/URLShortener/Controllers/ShortURLController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace URLShortener.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShortURLController : ControllerBase
+    {
+        private readonly ILogger<ShortURLController> _logger;
+
+        public ShortURLController(ILogger<ShortURLController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(10);
+            return Ok("tiny url");
+        }
+    }
+}
