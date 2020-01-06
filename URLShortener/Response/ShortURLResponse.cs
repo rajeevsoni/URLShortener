20533ed4b992767119f4573315007f4a7447787c
Rajeev Soni <rajeev.soni@onsolve.com>
2020-01-06 23:32:00 +0530

    added working endpoints for  URL Shortening service and redirecting

---
+++ URLShortener/Response/ShortURLResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace URLShortener.Response
+{
+    public class ShortURLResponse
+    {
+        public string ShortURL { get; set; }
+        public string Code { get; set; }
+    }
+}
--- URLShortener/Services/interfaces/IURLShortenerService.cs
+++ URLShortener/Services/interfaces/IURLShortenerService.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace URLShortener.Services
 {
-    interface IURLShortenerService
+    public interface IURLShortenerService
     {
-
+        Task<string> SaveURL(string url);
+        Task<string> GetURL(string hashedURL);
     }
 }
